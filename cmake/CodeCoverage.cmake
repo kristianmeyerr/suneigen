2f418858dc826bc8811b99797159caf09811a258
kristianmeyerr <km@meyerct.com>
2021-10-28 14:25:36 +0200

    Initial Commit

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -9,7 +9,7 @@
 # - Relax debug output.
 # - Keep a copy of the coverage output for later use.
 # - Updated coverage exclude patterns.
-# 
+#
 # 2018-01-03, HenryRLee
 # - Allow for *Clang compiler names, not just Clang.
 #
@@ -26,10 +26,10 @@
 # 2. Add the following line to your CMakeLists.txt:
 #      include(CodeCoverage)
 #
-# 3. Set compiler flags to turn off optimization and enable coverage: 
+# 3. Set compiler flags to turn off optimization and enable coverage:
 #      set(CMAKE_CXX_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
 #      set(CMAKE_C_FLAGS "-g -O0 -fprofile-arcs -ftest-coverage")
-#  
+#
 # 3. Use the function setup_target_for_coverage to create a custom make target
 #    which runs your test executable and produces a lcov code coverage report:
 #    Example:
@@ -42,53 +42,53 @@
 #      )
 #
 # 4. Build a Debug build:
-#	 cmake -DCMAKE_BUILD_TYPE=Debug ..
-#	 make
-#	 make my_coverage_target
+#     cmake -DCMAKE_BUILD_TYPE=Debug ..
+#     make
+#     make my_coverage_target
 
 # Param _targetname     The name of new the custom make target
 # Param _testrunner     The name of the target which runs the tests.
-#						MUST return ZERO always, even on errors. 
-#						If not, no coverage report will be created!
+#                        MUST return ZERO always, even on errors.
+#                        If not, no coverage report will be created!
 # Param _outputname     lcov output is generated as _outputname.info
 #                       HTML report is generated in _outputname/index.html
 # Optional fourth parameter is passed as arguments to _testrunner
 #   Pass them in list form, e.g.: "-j;2" for -j 2
 function(setup_target_for_coverage _targetname _testrunner _outputname)
 
-	if(NOT LCOV_PATH)
-		message(FATAL_ERROR "lcov not found! Aborting...")
-	endif() # NOT LCOV_PATH
+    if(NOT LCOV_PATH)
+        message(FATAL_ERROR "lcov not found! Aborting...")
+    endif() # NOT LCOV_PATH
 
-	if(NOT GENHTML_PATH)
-		message(FATAL_ERROR "genhtml not found! Aborting...")
-	endif() # NOT GENHTML_PATH
+    if(NOT GENHTML_PATH)
+        message(FATAL_ERROR "genhtml not found! Aborting...")
+    endif() # NOT GENHTML_PATH
 
-	# Setup target
-	add_custom_target(${_targetname}
-		
-		# Cleanup lcov
-		${LCOV_PATH} --directory . --zerocounters
-		
-		# Run tests
-		COMMAND ${_testrunner} ${ARGV3}
-		
-		# Capturing lcov counters and generating report
-		COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
+    # Setup target
+    add_custom_target(${_targetname}
+
+        # Cleanup lcov
+        ${LCOV_PATH} --directory . --zerocounters
+
+        # Run tests
+        COMMAND ${_testrunner} ${ARGV3}
+
+        # Capturing lcov counters and generating report
+        COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
 
         COMMAND ${LCOV_PATH} --remove ${_outputname}.info '*/tests/*' '/usr/*' '*/external/*' '*/3rd/*' '/Applications/*' --output-file ${_outputname}.info.cleaned
-		COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
+        COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
         COMMAND ${LCOV_PATH} --list ${_outputname}.info.cleaned
-		
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
-	)
-	
-	# Show info where to find the report
-	add_custom_command(TARGET ${_targetname} POST_BUILD
-		COMMAND ;
+
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
+    )
+
+    # Show info where to find the report
+    add_custom_command(TARGET ${_targetname} POST_BUILD
+        COMMAND ;
         COMMENT "${BoldMagenta}Open ./${_outputname}/index.html in your browser to view the coverage report.${ColourReset}"
-	)
+    )
 
 endfunction() # setup_target_for_coverage
 
@@ -136,8 +136,8 @@ if (cmake_build_type_tolower STREQUAL "coverage")
         CMAKE_SHARED_LINKER_FLAGS_COVERAGE )
 
 
-    # If unwanted files are included in the coverage reports, you can 
-    # adjust the exclude patterns on line 83. 
+    # If unwanted files are included in the coverage reports, you can
+    # adjust the exclude patterns on line 83.
     setup_target_for_coverage(
                             coverage            # Name for custom target.
                             ${TEST_MAIN}        # Name of the test driver executable that runs the tests.
@@ -145,10 +145,9 @@ if (cmake_build_type_tolower STREQUAL "coverage")
                                                 # otherwise the coverage generation will not complete.
                             coverage_out        # Name of output directory.
                             )
-else() 
-    add_custom_target(coverage 
-        COMMAND echo "${Red}Code coverage only available in coverage builds." 
-        COMMAND echo "${Green}Make a new build directory and rerun cmake with -DCMAKE_BUILD_TYPE=Coverage to enable this target.${ColorReset}" 
+else()
+    add_custom_target(coverage
+        COMMAND echo "${Red}Code coverage only available in coverage builds."
+        COMMAND echo "${Green}Make a new build directory and rerun cmake with -DCMAKE_BUILD_TYPE=Coverage to enable this target.${ColorReset}"
     )
 endif()
-

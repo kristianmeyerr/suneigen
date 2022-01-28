f54cec73363b4608b9314ce6b40d44fc7b519159
Kristian Meyer <km@meyerct.com>
2022-01-28 08:57:25 +0100

    Add windows CI support

--- tests/python/test_cvodes.py
+++ tests/python/test_cvodes.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 import suneigen4py
 import numpy as np
 from scipy.sparse import csc_matrix
+
 
 def test_cvodes_version():
     assert suneigen4py.__version__ == "0.0.1"
 
 
 def ode(t, x, p, k, h):
-    rhs = np.array([0., 0., 0.])
-    rhs[0] = -0.04 * x[0] + 1.0e4*x[1]*x[2]
+    rhs = np.array([0.0, 0.0, 0.0])
+    rhs[0] = -0.04 * x[0] + 1.0e4 * x[1] * x[2]
     rhs[2] = 3.0e7 * x[1] * x[1]
     rhs[1] = -rhs[0] - rhs[2]
     return rhs
@@ -17,9 +19,9 @@ def ode(t, x, p, k, h):
 def jac(t, x, p, k, h):
     jac_ = np.array(
         [
-            [-0.04, 1.0e4*x[2], 1.0e4*x[1]],
-            [0.04, (-1.0e4*x[2]) - (6.0e7*x[1]), -1.0e4*x[1]],
-            [0.0, 6.0e7*x[1], 0.0]
+            [-0.04, 1.0e4 * x[2], 1.0e4 * x[1]],
+            [0.04, (-1.0e4 * x[2]) - (6.0e7 * x[1]), -1.0e4 * x[1]],
+            [0.0, 6.0e7 * x[1], 0.0],
         ]
     )
     jac_sp = csc_matrix(jac_)
@@ -37,12 +39,12 @@ def test_cvodes_dense():
 
     t, x = suneigen4py.cvodes(ode, jac, x0, times, nnz, p, k, options)
 
-    np.testing.assert_almost_equal(x[0,0], 7.158017e-01, decimal=4)
-    np.testing.assert_almost_equal(x[0,1], 9.1850e-06, decimal=9)
-    np.testing.assert_almost_equal(x[0,2], 2.8418e-01, decimal=4)
-    np.testing.assert_almost_equal(x[1,0], 4.5053e-01, decimal=4)
-    np.testing.assert_almost_equal(x[1,1], 3.2232e-06, decimal=9)
-    np.testing.assert_almost_equal(x[1,2], 5.4946e-01, decimal=4)
+    np.testing.assert_almost_equal(x[0, 0], 7.158017e-01, decimal=4)
+    np.testing.assert_almost_equal(x[0, 1], 9.1850e-06, decimal=9)
+    np.testing.assert_almost_equal(x[0, 2], 2.8418e-01, decimal=4)
+    np.testing.assert_almost_equal(x[1, 0], 4.5053e-01, decimal=4)
+    np.testing.assert_almost_equal(x[1, 1], 3.2232e-06, decimal=9)
+    np.testing.assert_almost_equal(x[1, 2], 5.4946e-01, decimal=4)
 
 
 def test_cvodes_sparse():
@@ -56,9 +58,9 @@ def test_cvodes_sparse():
 
     t, x = suneigen4py.cvodes(ode, jac, x0, times, nnz, p, k, options)
 
-    np.testing.assert_almost_equal(x[0,0], 7.158017e-01, decimal=4)
-    np.testing.assert_almost_equal(x[0,1], 9.1850e-06, decimal=9)
-    np.testing.assert_almost_equal(x[0,2], 2.8418e-01, decimal=4)
-    np.testing.assert_almost_equal(x[1,0], 4.5053e-01, decimal=4)
-    np.testing.assert_almost_equal(x[1,1], 3.2232e-06, decimal=9)
-    np.testing.assert_almost_equal(x[1,2], 5.4946e-01, decimal=4)
+    np.testing.assert_almost_equal(x[0, 0], 7.158017e-01, decimal=4)
+    np.testing.assert_almost_equal(x[0, 1], 9.1850e-06, decimal=9)
+    np.testing.assert_almost_equal(x[0, 2], 2.8418e-01, decimal=4)
+    np.testing.assert_almost_equal(x[1, 0], 4.5053e-01, decimal=4)
+    np.testing.assert_almost_equal(x[1, 1], 3.2232e-06, decimal=9)
+    np.testing.assert_almost_equal(x[1, 2], 5.4946e-01, decimal=4)
